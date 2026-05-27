be3edb04011ac6e23f9608698e44d31250c103d6
Roger Presser de Paula <roger.presser.paula@gmail.com>
2026-05-27 19:26:54 -0300

    Validação das datas

--- MauiAppEventos/VIEWS/CadastroEvento.xaml.cs
+++ MauiAppEventos/VIEWS/CadastroEvento.xaml.cs
@@ -5,7 +5,14 @@ public partial class CadastroEvento : ContentPage
 	public CadastroEvento()
 	{
 		InitializeComponent();
-	}
+
+		dtpck_inicio.MinimumDate = DateTime.Now;
+		dtpck_inicio.MaximumDate = new DateTime(DateTime.Now.Year + 1, DateTime.Now.Month, DateTime.Now.Day);
+
+		dtpck_final.MinimumDate = dtpck_inicio.Date.Value.AddDays(1);
+        dtpck_final.MaximumDate = dtpck_inicio.Date.Value.AddDays(10);
+
+    }
 
     private void Button_Clicked(object sender, EventArgs e)
     {
@@ -19,4 +26,14 @@ public partial class CadastroEvento : ContentPage
             DisplayAlert("Ops", "ERRO!", "OK");
         }
     }
+    private void dtpck_inicio_DateSelected(object sender, DateChangedEventArgs e)
+    {
+        DatePicker elemento = sender as DatePicker;
+
+		DateTime data_selecionada_inicio = elemento.Date.Value;
+
+        dtpck_final.MinimumDate = data_selecionada_inicio.AddDays(1);
+        dtpck_final.MaximumDate = data_selecionada_inicio.AddDays(10);
+    }
+    
 }
